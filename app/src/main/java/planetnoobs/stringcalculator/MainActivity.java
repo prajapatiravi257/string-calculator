14aa2d19775183cd41b5c1ea85c082ea78e3c4cc
prajapatiravi257 <ravi.prajapati@1silverbullet.tech>
2021-09-09 00:14:46 +0530

    initial

--- app/src/main/java/planetnoobs/stringcalculator/MainActivity.java
+++ app/src/main/java/planetnoobs/stringcalculator/MainActivity.java
@@ -1,25 +1,56 @@
 package planetnoobs.stringcalculator;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
     TextInputLayout userInput;
+    MaterialButton btncalc;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         userInput = findViewById(R.id.userInput);
+        btncalc = findViewById(R.id.btncalc);
+
+        btncalc.setOnClickListener(view -> {
+            Integer total = calculateString(Objects.requireNonNull(userInput.getEditText()).getText().toString());
+            Toast.makeText(this, total.toString(), Toast.LENGTH_SHORT).show();
+
+        });
 
     }
 
-    private Integer calculateString(String input) {
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private int calculateString(String input) {
+        if(input.isEmpty()) {
+            return 0;
+        }
+        String delimiterRegex = "[,.-]+";
+        String[] list = input.split(delimiterRegex);
 
+        int total = 0;
+        for(String item: list) {
+            int num = Integer.parseInt(item);
+            total += num;
+        }
 
+        return total;
     }
 }
---
+++ app/src/test/java/planetnoobs/stringcalculator/StringValidator.java
@@ -0,0 +1,11 @@
+package planetnoobs.stringcalculator;
+
+import org.junit.Test;
+
+public class StringValidator {
+
+    @Test
+    public void validateString() {
+
+    }
+}
